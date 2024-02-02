93e5fd89d9ae2be7c74d5a821417b25a0b6cfb7f
fensteremil <carolafeierfeil@gmail.com>
2024-02-02 23:02:02 +0100

    Finished first two javascript test suite exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string = 'hey', count) {
+    let result = '';
 
+    if (count < 0) {
+        return 'ERROR';
+    }
+    else {
+        for (let i = 0; i < count; i++)
+        {
+            result += string;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
