f6c27c65b1bc35027de78cbf2093a3204df62287
fensteremil <carolafeierfeil@gmail.com>
2024-02-03 01:00:28 +0100

    Completed array manipulation and calculation exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...entries) {
+    // for (const entry of entries) {
+    //     const index = array.indexOf(entry);
+    //     if (index > -1) array.splice(index, 1);
+    // }
+    // return array;
+    return array.filter(value => !entries.includes(value));
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let result = 0;
 
+    if (min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') {
+        return 'ERROR';
+    }
+
+    if (min > max) {
+        const temp = max;
+        max = min;
+        min = temp;
+    }
+    
+    let sum = 0;
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
