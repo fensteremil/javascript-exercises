1800ed0d4181ce3a7384f3fc893ab1343cf17311
fensteremil <carolafeierfeil@gmail.com>
2024-02-15 17:20:16 +0100

    Finished Fibonacci Exercise (but recursively?)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n;
+    if (n >= 0) {
+        return n === 0 ? 0 : n === 1 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
+    } else return "OOPS";
 };
 
 // Do not edit below this line
