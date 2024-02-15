fa7ecefcab029a98f92c56796447364f424aff74
fensteremil <carolafeierfeil@gmail.com>
2024-02-15 17:43:57 +0100

    Finished Exercise 12 by reducing Array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((person, currentPerson) => {
+        const oldestAge = calcAge(person.yearOfBirth, person.yearOfDeath);
+        const currentAge = calcAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge > currentAge ? person : currentPerson;
+    }, {});
 };
+
+function calcAge(birth, death) {
+    let age = 0;
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
